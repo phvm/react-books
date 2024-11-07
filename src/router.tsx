193abba764168d02e39b832cbce96178fb9cc794
Pedro Moura <Pedrohvdm@gmail.com>
2024-11-07 12:29:55 -0300

    feat: router

---
+++ src/router.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { RouterLinks } from './constants';
+
+const Router = createBrowserRouter([
+  {
+    path: RouterLinks.home,
+    element: <></>,
+  },
+]);
+
+export default Router;
