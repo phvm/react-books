561a00b277777a2de8399153c226f5bac516e2a6
phvm <pedrohvdm@gmail.com>
2024-11-08 20:37:28 -0300

    feat: service to get by author and title

--- src/services/GoogleBooksService.ts
+++ src/services/GoogleBooksService.ts
@@ -5,7 +5,7 @@ const apiURL: string = 'https://www.googleapis.com/books/v1/volumes?';
 export async function getVolumes({
   query,
   searchLimit = 10,
-  orderBy = 'newest',
+  orderBy = 'relevance',
 }: {
   query: string;
   searchLimit?: number;
@@ -14,6 +14,31 @@ export async function getVolumes({
   const queryParams = new URLSearchParams({
     key: import.meta.env.VITE_API_KEY,
     q: query.replace(' ', '+'),
+    maxResults: searchLimit.toString(),
+    orderBy: orderBy,
+  });
+  try {
+    const response = await fetch(apiURL + queryParams);
+    return (await response.json()) as GoogleAPIResponse;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getByAuthorAndTitle({
+  title = '',
+  author = '',
+  searchLimit = 10,
+  orderBy = 'newest',
+}: {
+  title?: string;
+  author?: string;
+  searchLimit?: number;
+  orderBy?: string;
+}) {
+  const queryParams = new URLSearchParams({
+    key: import.meta.env.VITE_API_KEY,
+    q: `intitle:${title.replace(' ', '+')}+inauthor:${author.replace(' ', '+')}`,
     maxResults: searchLimit.toString(),
     orderBy: orderBy,
   });
