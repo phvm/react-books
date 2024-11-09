e4ce134689dec0643c765c47449e218a11e7bef5
phvm <pedrohvdm@gmail.com>
2024-11-09 18:41:30 -0300

    feat: routing for dashboard

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { RouterLinks } from './constants';
 import Home from './pages/Home';
+import Dashboard from './pages/dashboard';
 
 const Router = createBrowserRouter([
   {
     path: RouterLinks.home,
     element: <Home />,
+  },
+  {
+    path: RouterLinks.dashboard,
+    element: <Dashboard />,
   },
 ]);
 
