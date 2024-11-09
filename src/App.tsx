4d0d71ff66c11481275a565e431b4c81cb02af57
phvm <pedrohvdm@gmail.com>
2024-11-09 18:46:09 -0300

    feat: navbar on routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import { RouterProvider } from 'react-router-dom';
 import Router from './router';
+import Navbar from './components/Navbar';
+import { RouterLinks } from './constants.ts';
+
+const navbarLinks = [
+  { title: 'Home', link: RouterLinks.home },
+  { title: 'Dashboard', link: RouterLinks.dashboard },
+];
 
 function App() {
-  return <RouterProvider router={Router}></RouterProvider>;
+  return (
+    <>
+      <Navbar pages={navbarLinks} />
+      <RouterProvider router={Router}></RouterProvider>
+    </>
+  );
 }
 
 export default App;
