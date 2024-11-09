4816813532c28150c45d1eb1f5a60fb21b617856
phvm <pedrohvdm@gmail.com>
2024-11-09 18:09:30 -0300

    feat: display chart on home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,18 @@
 import { Container, InputsContainer } from './styles.ts';
 import BooksTable from '../../components/Table';
 import { useEffect, useState } from 'react';
-import { getByAuthorAndTitle, getVolumes } from '../../services/GoogleBooksService.ts';
+import { getBooksByCategory, getByAuthorAndTitle, getVolumes } from '../../services/GoogleBooksService.ts';
 import { APIBook } from '../../types/apiTypes.ts';
-import { Book } from '../../types/commonTypes.ts';
+import { Book, CategoryRatings } from '../../types/commonTypes.ts';
 import SearchFilter from '../../components/SearchFilter';
+import CategoriesChart from '../../components/CategoriesChart';
 
 export default function Home() {
   const [books, setBooks] = useState<Book[]>([]);
   const [author, setAuthor] = useState<string>('');
   const [title, setTitle] = useState<string>('');
+  const [categoryRatings, setCategoryRatings] = useState<CategoryRatings[]>([]);
+  const exampleCategories = ['fiction', 'romance', 'medical', 'engineering', 'cooking'];
 
   function onTitleChange(title: string): void {
     setTitle(title);
@@ -22,20 +25,49 @@ export default function Home() {
   }
 
   async function getBookByAuthorAndTitle(author: string, title: string): Promise<void> {
-    const response = await getByAuthorAndTitle({ author, title });
+    const response = await getByAuthorAndTitle({ author, title, searchLimit: 40 });
     if (response) {
       const data: Book[] = response.items.map((item) => {
         return {
           id: item.id,
           title: item.volumeInfo.title,
           author: item.volumeInfo.authors ? item.volumeInfo.authors.join(', ') : 'Indisponivel',
-          categories: item.volumeInfo.categories ? item.volumeInfo.categories.join(', ') : 'Indisponivel',
+          categories: item.volumeInfo?.categories?.join() ?? 'Indisponivel',
           avgRating: item.volumeInfo.averageRating ?? 'Indisponível',
         };
       });
       setBooks(data);
     }
   }
+
+  useEffect(() => {
+    async function getRatingsByCategories(categories: string[]) {
+      for (const category of categories) {
+        const rates: number[] = [0, 0, 0, 0, 0, 0];
+        const response = await getBooksByCategory({ category: category });
+        if (response !== undefined) {
+          response.items
+            .filter((item) => !!item.volumeInfo.averageRating)
+            .forEach((ratedItem) => {
+              rates[Math.round(ratedItem.volumeInfo.averageRating)] += 1;
+            });
+          setCategoryRatings((prevState) =>
+            prevState.some((item) => item.category === category)
+              ? [...prevState]
+              : [
+                  ...prevState,
+                  {
+                    category: category,
+                    ratings: rates,
+                  },
+                ],
+          );
+        }
+      }
+    }
+
+    getRatingsByCategories(exampleCategories);
+  }, []);
 
   useEffect(() => {
     async function getBooks() {
@@ -73,8 +105,8 @@ export default function Home() {
           helperText="Inserir o autor do livro"
         />
       </InputsContainer>
-
       <BooksTable books={books} />
+      <CategoriesChart ratingsData={categoryRatings} />
     </Container>
   );
 }
